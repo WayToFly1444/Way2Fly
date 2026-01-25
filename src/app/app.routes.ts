f3f03440c59ec1e4ad94a5a6b43b5a237076e104
srikanthroyal241@gmail.com <Srikanth@143>
2026-01-25 17:09:57 +0530

    .

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,15 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home-component/home-component';
 
 export const routes: Routes = [
-    {path: '', redirectTo: '/home', pathMatch: 'full' },
-    {path: 'home', component: HomeComponent },
-    {path:'', loadChildren: () => import('./components/home-routes/home.routes').then(m => m.routes) }
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+
+  { path: 'home', component: HomeComponent },
+
+  {
+    path: '',
+    loadChildren: () =>
+      import('./components/home-routes/home.routes').then(m => m.routes)
+  },
+
+  { path: '**', redirectTo: 'home' }
 ];
